ee6740bd55fd161380cf49431ba3f7d833101f80
Raul Olmedo <100346073@alumnos.uc3m.es>
2017-10-10 23:38:34 +0200

    Added a bunch of TODOs

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/*
+* THINGS TO IMPLEMENT
+*
+* TODO hide the navbar when scrolling
+* TODO fix the name section in the background to then make it disappear when scrolled (below the card)
+* TODO animation when loading the page
+* TODO move the profile picture to the left when scrolling
+* TODO move the profile section to the right when scrolling
+* TODO put the cards above the navbar when scrolling
+* TODO animate FAB
+* TODO when the end is reached, remark the CV download of the FAB
+* TODO animate when cards appear and when they disappear -swipe left, right, etc-
+*
+* */
